4ff2aed8ba5946fc332a7781879509bc4ac0f800
xayon40-12 <touroux.nathan@gmail.com>
2023-03-23 11:40:38 +0900

    generate trento for b=0,3,7 and sigma=4.23,6.4,7.32

--- utils/gen_trento.hs
+++ utils/gen_trento.hs
@@ -2,5 +2,10 @@
 
 for i in $@; do
   dx=$(echo 20 / $i | bc -l)
-  trento Pb Pb --random-seed 1 --b-min 7 --b-max 7 --grid-max 10 --grid-step $dx 100 -o e$i
+  for b in 0 3 7; do
+    for sig in 4.23 6.4 7.32; do
+      mkdir -p sig$sig/b$b/e$i
+      trento Pb Pb --random-seed 1 --b-min $b --b-max $b --cross-section $sig --grid-max 10 --grid-step $dx 100 -o sig$sig/b$b/e$i
+    done
+  done
 done
