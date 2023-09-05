48a058b3165f74c08c9986af60569396976c7d38
xayon40-12 <touroux.nathan@gmail.com>
2023-09-05 17:24:22 +0900

    change threshold

--- src/hydro/viscous/viscous2d.rs
+++ src/hydro/viscous/viscous2d.rs
@@ -550,8 +550,7 @@ pub fn viscous2d<const V: usize, const S: usize>(
         freezeout_energy: Some(freezeout_energy),
     };
 
-    // let e = 2e-3;
-    let e = 1e-1;
+    let e = 1e-3;
     let err_thr = |_t: f64,
                    vs: &[[[[f64; F_BOTH_2D]; V]; V]; 1],
                    _trs: &[[[[f64; C_MILNE_BOTH_2D]; V]; V]; 1]| {
