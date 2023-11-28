b0406a9e54f2b0fbe581dca131d455c8d7e204d3
xayon40-12 <touroux.nathan@gmail.com>
2023-11-28 17:53:22 +0900

    add vn and jacknife

--- analysis/src/main.rs
+++ analysis/src/main.rs
@@ -1,18 +1,19 @@
+use num::Complex;
 use std::{collections::HashMap, iter::repeat};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Particle {
     pub eta: f64,
     pub pt: f64,
     pub phi: f64,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct FreezeOut {
     pub particles: Vec<Particle>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Event {
     pub freezeouts: Vec<FreezeOut>,
 }
@@ -42,39 +43,59 @@ impl Event {
 //    else:
 //       return value, 0
 
-// def vn(n, events):
-//    events = [[[v for v in f if abs(v["eta"])<0.8 and 0.2 < v["pT"] and v["pT"] < 5.0 and v["charge"] != 0] for f in e] for e in events]
+pub fn jacknife<F: Fn(f64, f64) -> f64>(f: F, aa: Vec<f64>, bb: Vec<f64>) -> (f64, f64) {
+    let a: f64 = aa.iter().sum();
+    let b: f64 = bb.iter().sum();
+    let val = f(a, b);
+    let l = aa.len();
+    if l > 1 {
+        let fs: Vec<f64> = aa
+            .iter()
+            .zip(bb.iter())
+            .map(|(ia, ib)| f(a - ia, b - ib))
+            .collect();
+        let fh = fs.iter().cloned().sum::<f64>() / l as f64;
+        let vh = fs.iter().map(|f| (f - fh).powi(2)).sum::<f64>() / l as f64;
+        let err = (l - 1) as f64 * vh;
+        (val, err)
+    } else {
+        (val, 0.0)
+    }
+}
 
-//    def fun(n, d):
-//       return np.sqrt(n/d)
+pub fn vn(n: f64, events: &Vec<&Event>) -> (f64, f64) {
+    let (nums, dens) = events
+        .iter()
+        .map(|e| {
+            e.freezeouts
+                .iter()
+                .map(|f| {
+                    let particles: Vec<_> = f
+                        .particles
+                        .iter()
+                        .filter(|p| p.eta.abs() < 0.8 && 0.2 < p.pt && p.pt < 5.0)
+                        .cloned()
+                        .collect();
 
-//    mss = [[len(f) for f in e] for e in events]
+                    let m = particles.len() as f64;
+                    let qn2 = particles
+                        .iter()
+                        .map(|p| Complex::new(0.0, n * p.phi))
+                        .sum::<Complex<f64>>()
+                        .norm_sqr();
 
-//    qnss = [[sum(exp(1j*n*v["phi"]) for v in f) for f in e] for e in events]
-//    qn2ss = [[abs(qn)**2 for qn in qns] for qns in qnss]
+                    (qn2 - m, m * (m - 1.0))
+                })
+                .fold((0.0, 0.0), |(num, den), (m2, w2)| (num + m2, den + w2))
+        })
+        .fold((vec![], vec![]), |(mut nums, mut dens), (num, den)| {
+            nums.push(num);
+            dens.push(den);
+            (nums, dens)
+        });
 
-//    w2ss = [[m*(m-1) for m in ms] for ms in mss]
-
-//    m2ss = [[(qn2-m)/w2 for qn2, m, w2 in zip(qn2s, ms, w2s)] for qn2s, ms, w2s in zip(qn2ss, mss, w2ss)]
-
-//    n_cn2s = [sum(w2*m2 for w2, m2 in zip(w2s, m2s)) for w2s, m2s in zip(w2ss, m2ss)]
-//    d_cn2s = [sum(w2s) for w2s in w2ss]
-
-//    val, err = jackknife(fun, n_cn2s, d_cn2s)
-
-//    return val, err
-
-// def dch_deta_eta(events):
-//    deta = 0.25
-//    ran = np.arange(0, 5, deta)
-//    etas = [eta+deta/2 for eta in ran]
-//    dchdetass = [[len([v for v in f if eta <= abs(v["eta"]) and abs(v["eta"]) <= eta+deta and v["charge"] != 0])/(2*deta) for e in events for f in e] for eta in ran] # abs and /2 because it is symetrized
-//    dchdetas = [sum(dchdetas)/len(dchdetas) for dchdetas in dchdetass]
-//    dchdetas2 = [sum(x**2 for x in dchdetas)/len(dchdetas) for dchdetas in dchdetass]
-//    errdchdetas = [np.sqrt((d2-d**2)/len(events)) for d, d2 in zip(dchdetas,dchdetas2)]
-//    plt.errorbar(etas, dchdetas, yerr=errdchdetas)
-//    plt.ylim([0,2500])
-//    plt.savefig("dch_deta_eta.pdf")
+    jacknife(|num, den| (num / den).sqrt(), nums, dens)
+}
 
 // Simetrized dn/deta os function of eta
 pub fn dn_deta_eta(events: &Vec<&Event>) -> Vec<(f64, f64, f64)> {
@@ -200,10 +221,10 @@ fn main() {
 
     // Choose the number of bins
     let l = mults.len();
-    let sizes = repeat(l / 40).take(4).chain(repeat(l / 10).take(7));
+    let sizes = repeat(l / 40).take(4).chain(repeat(l / 10).take(9));
     let percent = |i| i as f64 / l as f64 * 100.0;
 
-    let mut msg = String::new();
+    let mut msg = vec![String::new(); 3];
     let mut j = 0;
     // For each bin
     for size in sizes {
@@ -214,9 +235,9 @@ fn main() {
         let events: Vec<&Event> = (0..size).map(|k| &events[mults[j + k].1]).collect();
 
         let (dndeta, errdndeta) = dn_deta(&events);
-        msg = format!(
+        msg[0] = format!(
             "{}dn/deta-mid|{:.1}-{:.1}|{:.0}:{:.1}\n",
-            msg, bin.0, bin.1, dndeta, errdndeta
+            msg[0], bin.0, bin.1, dndeta, errdndeta
         );
         let dndetaetas = dn_deta_eta(&events);
         let dndetaetas_data = dndetaetas
@@ -224,13 +245,20 @@ fn main() {
             .map(|(eta, dndeta, errdndeta)| format!("{:.3}:{:.0}:{:.1}", eta, dndeta, errdndeta))
             .collect::<Vec<_>>()
             .join(" ");
-        msg = format!(
+        msg[1] = format!(
             "{}dn/deta|{:.1}-{:.1}|{}\n",
-            msg, bin.0, bin.1, dndetaetas_data
+            msg[1], bin.0, bin.1, dndetaetas_data
+        );
+        let (v2, errv2) = vn(2.0, &events);
+        msg[2] = format!(
+            "{}v2|{:.1}-{:.1}|{:.3}:{:.3}\n",
+            msg[2], bin.0, bin.1, v2, errv2
         );
 
         j += size;
     }
+
+    let msg = msg.join("\n");
     println!("{}", msg);
     std::fs::write("observables.txt", msg).expect("Could not write to observables.txt");
 }
