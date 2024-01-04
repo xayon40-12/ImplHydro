50d50d49c2d3eefbbad336ef6d08779e46057ab1
xayon40-12 <touroux.nathan@gmail.com>
2024-01-04 17:21:08 +0100

    load and plot experimental data

--- utils/observables.py
+++ utils/observables.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import os
 import numpy as np
+import yaml
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -31,8 +33,16 @@ def parse(lines):
 def plot_dndeta_eta(dndeta_eta):
     plt.close()
     name, centralities, valuess = dndeta_eta
+    max_c = 60
     for [cl,cr], (pos, value, err) in zip(centralities, valuess):
-        plt.plot(pos, value, label="{}%-{}%".format(cl,cr))
+        if cl < max_c:
+            plt.plot(pos, value, label="{}%-{}%".format(cl,cr))
+    fst = True
+    for [cl,cr], (pos, value, err) in alice5020["dndeta-eta"]:
+        if cl < max_c:
+            name = fst and "ALICE 5.02TeV" or ""
+            fst = False
+            plt.plot(pos, value, "o", color="black", label=name)
     plt.xlabel("$\eta$")
     plt.ylabel("$\mathrm{dN}_{\mathrm{ch}}/\mathrm{d}\eta$")
     plt.legend()
@@ -40,36 +50,96 @@ def plot_dndeta_eta(dndeta_eta):
 
 def plot_dndeta_mid(dndeta_mid):
     plt.close()
-    alice5TeV_centralities = [[0,2.5],[2.5,5.0],[5.0,7.5],[7.5,10],[10,20],[20,30],[30,40],[40,50],[50,60],[60,70],[70,80]]
-    alice5TeV_values = [[2035, 52],[1850, 55],[1666, 48],[1505, 44],[1180, 31],[786, 20],[512, 15],[318, 12],[183, 8],[96.3, 5.8],[44.9, 3.4]]
 
     name, centralities, valuess = dndeta_mid
+    centralities_c = [(l+r)/2 for [l,r] in centralities]
+    values = [v[0] for v in valuess]
+    errs = [v[1] for v in valuess]
+    plt.plot(centralities_c, values, "-", label="numerics")
 
-    for name, shape, centralities, valuess in [("numerics", "-", centralities, valuess), ("Alice 5.02TeV", "o", alice5TeV_centralities, alice5TeV_values)]:
-        centralities_c = [(l+r)/2 for [l,r] in centralities]
-        values = [v[0] for v in valuess]
-        errs = [v[1] for v in valuess]
-        plt.plot(centralities_c, values, shape, label=name)
+    centralities, valuess = alice5020["dndeta"]
+    centralities_c = [(l+r)/2 for [l,r] in centralities]
+    values = [v[0] for v in valuess]
+    errs = [v[1] for v in valuess]
+    plt.plot(centralities_c, values, "o", color="black", label="ALICE 5.02TeV")
+
     plt.xlabel("centrality (%)")
     plt.ylabel("$\mathrm{dN}_{\mathrm{ch}}/\mathrm{d}\eta$")
     plt.legend()
     plt.savefig("dndeta-mid.pdf")
 
 def plot_vn(vns):
-    alice5TeV_centralities = [[0,5],[5.0,10],[10,20],[20,30],[30,40],[40,50],[50,60],[60,70],[70,80]]
-    alice5TeV_values = [[0.029, 0], [0.047, 0], [0.065, 0], [0.088, 0], [0.099, 0], [0.105, 0], [0.103, 0], [0.098, 0], [0.089, 0]]
-    alice = [("Alice 5.02TeV", "o", alice5TeV_centralities, alice5TeV_values)]
+    alice = [alice5020[n] for (n, _, _) in vns]
     vns = [("$v_{{{}}}${{2}}".format(n[1]), "-", c, v) for (n, c, v) in vns]
     plt.close()
-    for name, shape, centralities, valuess in vns+alice:
+    for name, shape, centralities, valuess in vns:
         centralities_c = [(l+r)/2 for [l,r] in centralities]
         values = [v[0] for v in valuess]
         errs = [v[1] for v in valuess]
         plt.plot(centralities_c, values, shape, label=name)
+    fst = True
+    for centralities, valuess in alice:
+        centralities_c = [(l+r)/2 for [l,r] in centralities]
+        values = [v[0] for v in valuess]
+        errs = [v[1] for v in valuess]
+        name = fst and name or ""
+        fst = False
+        plt.plot(centralities_c, values, "o", color="black", label=name)
     plt.xlabel("centrality (%)")
     plt.ylabel("$v_n${2}")
     plt.legend()
     plt.savefig("vn.pdf")
+
+def tosym(err):
+    if "asymerror" in err:
+        e = err["asymerror"]
+        return max(-e["minus"],e["plus"])
+    else:
+        return err["symerror"]
+
+alice5020_path = os.environ.get("ALICE5020",".")
+
+alice5020 = {"v2": None, "v3": None, "v4": None, "dndeta": None, "dndeta-eta": None}
+with open(alice5020_path+"/ALICE_5.02TeV-v2.yaml", "r") as f:
+    y = yaml.safe_load(f)
+    ivs = y["independent_variables"][0]["values"]
+    cs = [[v[d] for d in ["low","high"]] for v in ivs]
+    dvs = y["dependent_variables"][0]["values"]
+    vs = [v["value"] for v in dvs]
+    errs = [sum(v["errors"][i]["symerror"] for i in [0,1]) for v in dvs]
+    alice5020["v2"] = (cs, list(zip(vs, errs)))
+
+with open(alice5020_path+"/ALICE_5.02TeV-v3-v4.yaml", "r") as f:
+    y = yaml.safe_load(f)
+    ivs = y["independent_variables"][0]["values"]
+    cs = [[v[d] for d in ["low","high"]] for v in ivs]
+    for v, n in [(0, "v3"), (1, "v4")]:
+        dvs = y["dependent_variables"][v]["values"]
+        vs = [v["value"] for v in dvs]
+        errs = [sum(v["errors"][i]["symerror"] for i in [0,1]) for v in dvs]
+        alice5020[n] = (cs, list(zip(vs, errs)))
+
+with open(alice5020_path+"/ALICE_5.02TeV-dndeta.yaml", "r") as f:
+    y = yaml.safe_load(f)
+    ivs = y["independent_variables"][0]["values"]
+    cs = [[v[d] for d in ["low","high"]] for v in ivs]
+    dvs = y["dependent_variables"][0]["values"]
+    vs = [v["value"] for v in dvs]
+    errs = [sum(v["errors"][i]["symerror"] for i in [0,1]) for v in dvs]
+    alice5020["dndeta"] = (cs, list(zip(vs, errs)))
+    
+with open(alice5020_path+"/ALICE_5.02TeV-dndeta-eta.yaml", "r") as f:
+    y = yaml.safe_load(f)
+    ivs = y["independent_variables"][0]["values"]
+    etas = [sum(v[d] for d in ["low","high"])/2 for v in ivs]
+    dndeta_eta = []
+    for ci in range(len(y["dependent_variables"])):
+        dvs = y["dependent_variables"][ci]["values"]
+        c = [float(v) for v in y["dependent_variables"][ci]["qualifiers"][0]["value"].split("-")]
+        vs = [v["value"] for v in dvs]
+        errs = [v["errors"][0]["symerror"]+tosym(v["errors"][1]) for v in dvs]
+        dndeta_eta += [(c, (etas, vs, errs))]
+    alice5020["dndeta-eta"] = dndeta_eta
 
 with open("observables.txt") as f:
     obs = [parse(lines) for lines in f.read().split("\n\n")]
